09f00b7be218c10cf4238246541a030488ef0542
Kabeer_Acesoft <kabeer.acesoft@gmail.com>
2025-11-17 11:57:25 +0530

    fixed images 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export" വേണ്ട
-  // basePath വേണ്ട
-  // assetPrefix വേണ്ട
   images: {
-    // ഇത് വേണമെങ്കിൽ വെക്കാം, ഇല്ലെങ്കിലും പ്രശ്നമില്ല
-    unoptimized: true,
+    unoptimized: true,   // keep this if you want, it’s fine
   },
 };
 
